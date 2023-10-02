d650237906b7be320582112273371fa414b23418
AltairFer2 <20031003@itcelaya.edu.mx>
2023-10-01 21:19:46 -0600

    Versión Con Tabla

--- src/RompecabezasGUI.java
+++ src/RompecabezasGUI.java
@@ -1,28 +1,42 @@
 import javax.swing.*;
+import javax.swing.Timer;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.*;
+import java.util.Queue;
+import java.util.List;
 
 public class RompecabezasGUI extends JFrame {
-    private JTextArea outputTextArea;
+    private JTable puzzleTable; // Usar JTable para mostrar la matriz
     private int[][] estadoInicial;
     private int[][] estadoFinal;
+    private List<String> iteraciones; // Lista para almacenar las iteraciones
+    private int currentIterationIndex; // Índice de la iteración actual
+    private DefaultTableModel tableModel;
 
     public RompecabezasGUI() {
         // Configurar la ventana
         setTitle("Resolución de Rompecabezas 8-Puzzle");
-        setSize(400, 300);
+        setSize(800, 800);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
 
         // Crear un panel para colocar componentes
         JPanel panel = new JPanel();
         panel.setLayout(new BorderLayout());
 
-        // Crear un área de texto para mostrar la salida
-        outputTextArea = new JTextArea();
-        outputTextArea.setEditable(false);
-        JScrollPane scrollPane = new JScrollPane(outputTextArea);
+        // Crear un JTable para mostrar la matriz
+        tableModel = new DefaultTableModel(3, 3);
+        puzzleTable = new JTable(tableModel);
+        puzzleTable.setCellSelectionEnabled(false);
+        puzzleTable.setRowHeight(50);
+        puzzleTable.setFont(new Font("Arial", Font.PLAIN, 24));
+
+        // Agregar el JTable a un JScrollPane
+        JScrollPane scrollPane = new JScrollPane(puzzleTable);
 
         // Crear un botón para iniciar la búsqueda
         JButton startButton = new JButton("Iniciar Búsqueda");
@@ -36,27 +50,12 @@ public class RompecabezasGUI extends JFrame {
                 SwingWorker<Void, String> worker = new SwingWorker<Void, String>() {
                     @Override
                     protected Void doInBackground() {
-                        // Coloca tu lógica de búsqueda aquí y publica resultados parciales
-                        publish("Comenzando la búsqueda...\n");
-
+                        // Limpiar iteraciones anteriores
+                        iteraciones.clear();
+                        currentIterationIndex = 0;
                         // Ejemplo: Búsqueda ciega en anchura
                         busquedaCiegaAnchura(estadoInicial, estadoFinal);
-
                         return null;
-                    }
-
-                    @Override
-                    protected void process(java.util.List<String> chunks) {
-                        // Actualizar el área de texto con resultados parciales
-                        for (String chunk : chunks) {
-                            outputTextArea.append(chunk);
-                        }
-                    }
-
-                    @Override
-                    protected void done() {
-                        // Habilitar el botón nuevamente cuando la búsqueda haya terminado
-                        startButton.setEnabled(true);
                     }
                 };
 
@@ -83,71 +82,68 @@ public class RompecabezasGUI extends JFrame {
                 { 4, 5, 6 },
                 { 7, 8, 0 }
         };
+
+        // Inicializar la lista de iteraciones
+        iteraciones = new ArrayList<>();
+
+        // Inicializar la tabla con el estado inicial
+        updateTableModel(estadoInicial);
     }
 
     public void busquedaCiegaAnchura(int[][] estadoInicial, int[][] estadoFinal) {
-        boolean encontro = false;// variable para declarar si encontro solucion
-
-        // se declara el nodo current o actual y se almacena el estadoInicial
+        boolean encontro = false;
         RompecabezasEstado actual = new RompecabezasEstado(estadoInicial);
-
-        // Para almacenar nodos hijos
         RompecabezasEstado hijo = null;
-
-        // lista closed (para nodos cerrados) -- cola
         Queue<RompecabezasEstado> cerrado = new LinkedList<>();
-        // lita open (para nodos abiertos) -- hash se empleara para evitar duplicados
-        // almacenando
-        // los nodos abiertos aqui y reducir nodos duplicados
         Set<RompecabezasEstado> abierto = new HashSet<>();
-
-        // pila empleada para almacenar la ruta solucion
         Stack<RompecabezasEstado> solucion = null;
-
-        // colocal nodo inicial en cerrados
         cerrado.offer(actual);
 
-        // mientras cerrado no este vacion o no se haya encontrado el estado final
         while (!cerrado.isEmpty() && (!encontro)) {
-            actual = cerrado.poll();// sacar primer nodo de lista(cola)
-            if (!abierto.contains(actual))// comprobar si el nodo actual esta en la lista de nodos abiertos
-            {
-                abierto.add(actual);// añadir a lista de nodos abiertos
+            actual = cerrado.poll();
+            if (!abierto.contains(actual)) {
+                abierto.add(actual);
 
-                // iterar mientras existan movimientos y no se haya encontrado el estado final
                 for (int i = 0; (i < 4) && (!encontro); i++) {
-                    hijo = movimientos(actual, i);// realizar movimientos
+                    hijo = movimientos(actual, i);
                     if (hijo != null) {
-                        cerrado.offer(hijo);// añadir nodo hijo al final de la lista(cola)
-                        encontro = comparar(estadoFinal, hijo.estado);// comprobar si el nodo hijo es el estado final
+                        cerrado.offer(hijo);
+                        encontro = comparar(estadoFinal, hijo.estado);
                     }
                 }
+
+                // Agregar la iteración formateada a la lista
+                iteraciones.add(imprimirMatriz(actual.estado));
             }
         }
 
-        // se encontro el estado final
-        if (encontro)// si se encontro se imprime el camino
-        {
-            solucion = new Stack<>();// se inicializa la pila
-            while (hijo != null)// mientras nodo hijo no sea nullo
-            {
-                solucion.push(hijo);// se mete el nodo a superficie de la pila
-                hijo = hijo.padre;// el nodo se mueve al nodo padre
-                // como es una pila el el estado final va a terminar al final de la pila y el
-                // inicial al comienzo
+        if (encontro) {
+            solucion = new Stack<>();
+            while (hijo != null) {
+                solucion.push(hijo);
+                hijo = hijo.padre;
             }
-            // imprimir camino
-            while (!solucion.isEmpty()) {// mientras la pila no este vacia
-                actual = solucion.pop();// sacar elemento de la pila
-                imprimirMatriz(actual.estado);// imprimir nodo o matriz
-            }
-        } else// si no se encontro, se e¡imprime el mensaje de salida
-        {
-            System.out.println("No existe un camino para llegar del estado inicial al final");
+
+            // Usar un temporizador para mostrar las iteraciones con pausas
+            Timer timer = new Timer(500, new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (currentIterationIndex < iteraciones.size()) {
+                        mostrarIteracion(iteraciones.get(currentIterationIndex));
+                        currentIterationIndex++;
+                    } else {
+                        // Cuando se muestran todas las iteraciones, detener el temporizador
+                        ((Timer) e.getSource()).stop();
+                    }
+                }
+            });
+            timer.start();
+        } else {
+            // Si no se encontró una solución
+            mostrarIteracion("No existe un camino para llegar del estado inicial al final");
         }
     }
 
-    // para realizar movimientos en el rompecabezas
     public RompecabezasEstado movimientos(RompecabezasEstado estado, int mov) {
         int columna = estado.columnaBlanca;
         int fila = estado.filaBlanca;
@@ -177,15 +173,15 @@ public class RompecabezasGUI extends JFrame {
                 }
                 break;
         }
-        int[][] nuevaM = replicarMatriz(estado.estado);// replicar matriz
-        nuevaM[estado.filaBlanca][estado.columnaBlanca] = nuevaM[fila][columna];// movimiento de vacio a valor
-        nuevaM[fila][columna] = 0;// movimiento de estado valor a vacio
-        RompecabezasEstado hijo = new RompecabezasEstado(nuevaM, fila, columna, estado);// crear nodo hijo
+        int[][] nuevaM = clonarMatriz(estado.estado);
+        nuevaM[estado.filaBlanca][estado.columnaBlanca] = nuevaM[fila][columna];
+        nuevaM[fila][columna] = 0;
+        RompecabezasEstado hijo = new RompecabezasEstado(nuevaM, fila, columna, estado);
         return hijo;
     }
 
-    // para crear una replica de una matriz
-    public int[][] replicarMatriz(int[][] matriz) {
+    // para clonar una matriz
+    public int[][] clonarMatriz(int[][] matriz) {
         int[][] nuevaMatriz = new int[matriz.length][matriz[0].length];
         for (int fila = 0; fila < matriz.length; fila++) {
             for (int columna = 0; columna < matriz[fila].length; columna++) {
@@ -195,28 +191,55 @@ public class RompecabezasGUI extends JFrame {
         return nuevaMatriz;
     }
 
-    // para comparar dos matrices y saber si son iguales
     public boolean comparar(int[][] meta, int[][] hijo) {
         for (int fila = 0; fila < meta.length; fila++) {
             for (int columna = 0; columna < meta[fila].length; columna++) {
-                if (meta[fila][columna] != hijo[fila][columna])// si hay una diferencia no son iguales
-                {
+                if (meta[fila][columna] != hijo[fila][columna]) {
                     return false;
                 }
             }
         }
-        // si no encontro diferencias son iguales
         return true;
     }
 
-    public void imprimirMatriz(int[][] matriz) {
+    public String imprimirMatriz(int[][] matriz) {
+        StringBuilder sb = new StringBuilder();
         for (int fila = 0; fila < matriz.length; fila++) {
             for (int columna = 0; columna < matriz[fila].length; columna++) {
-                outputTextArea.append(matriz[fila][columna] + "  ");
+                sb.append(matriz[fila][columna]).append("  ");
             }
-            outputTextArea.append("\n");
+            sb.append("\n");
         }
-        outputTextArea.append("\n");
+        sb.append("\n");
+        return sb.toString();
+    }
+
+    public void mostrarIteracion(String iteracion) {
+        // Limpiar la tabla antes de mostrar la nueva iteración
+        clearTableModel();
+        String[] filas = iteracion.split("\n");
+        for (int i = 0; i < filas.length; i++) {
+            String[] numeros = filas[i].trim().split("\\s+");
+            for (int j = 0; j < numeros.length; j++) {
+                tableModel.setValueAt(Integer.parseInt(numeros[j]), i, j);
+            }
+        }
+    }
+
+    public void clearTableModel() {
+        for (int row = 0; row < tableModel.getRowCount(); row++) {
+            for (int col = 0; col < tableModel.getColumnCount(); col++) {
+                tableModel.setValueAt("", row, col);
+            }
+        }
+    }
+
+    public void updateTableModel(int[][] matriz) {
+        for (int row = 0; row < tableModel.getRowCount(); row++) {
+            for (int col = 0; col < tableModel.getColumnCount(); col++) {
+                tableModel.setValueAt(matriz[row][col], row, col);
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -229,21 +252,17 @@ public class RompecabezasGUI extends JFrame {
     }
 }
 
-// Se crea esta clase para almacenar la matriz, y un apuntador al nodo padre
-// ademas de almacenar el espacio en blanco
 class RompecabezasEstado {
     public int[][] estado;
     public int columnaBlanca;
     public int filaBlanca;
     public RompecabezasEstado padre;
 
-    // constructor empleado para nodo inicial
     public RompecabezasEstado(int[][] estado) {
         this.estado = estado;
-        encontrarEspacioBlanco();// encontrar espacio blanco para nodo inicial
+        encontrarEspacioBlanco();
     }
 
-    // constructor empleado para nodo hijo
     public RompecabezasEstado(int[][] estado, int filaBlanca, int columnaBlanca, RompecabezasEstado padre) {
         this.estado = estado;
         this.columnaBlanca = columnaBlanca;
@@ -283,4 +302,5 @@ class RompecabezasEstado {
     public String toString() {
         return Arrays.deepToString(estado);
     }
+
 }
